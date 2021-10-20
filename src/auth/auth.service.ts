614db2282875755d8317ea7e1fa0d50665b719aa
fresnel <ngomafresnel@gmail.com>
2021-10-20 13:21:36 +0100

    Modification de auth.service login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,50 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
 import { LoginDTO, RegisterDTO } from 'src/models/user.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-    private nockUser = {
-        email:"billy@gmail.com",
-        token:"jwt.token",
-        username:"bill",
-        bio:"billlll",
-        image: null
-    }
+    constructor(
+        @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>
+    ){}
 
-    register(credentials : RegisterDTO){
-        return this.nockUser;
+     async register(credentials : RegisterDTO){
+       try {
+           const user = this.userRepo.create(credentials);
+           await user.save();
+           return user;
+       } catch (error) {
+           if(error.code === '23505'){
+               throw new ConflictException('Username has already been taken');
+           }
+           throw new InternalServerErrorException();   
+       }
     }
     
-    login(credentials : LoginDTO){
-        if(credentials.email === this.nockUser.email){
-            return this.nockUser;
+    async login({email, password} : LoginDTO){
+       try {
+           const user = await this.userRepo.findOne({where: {email} });
+           const isValid = await user.comparePassword(password);
+           if(!isValid){
+            throw new UnauthorizedException('Invalid credentials'); 
+           }
+           /*if(user && (await user.comparePassword(password)) ) {
+                return user;
+           }*/
+           return user;
+       } catch (error) {
+        if (error.response)
+        {
+            throw error;
         }
-        throw new InternalServerErrorException();
+        else
+        {
+            throw new InternalServerErrorException();
+        }
+        //throw new UnauthorizedException('Invalid credentials'); 
+       } 
     }
 
 
